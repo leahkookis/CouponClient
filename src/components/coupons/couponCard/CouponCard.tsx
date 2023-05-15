4924134fe000c1b3922c7180f05a02eb22817c1b
lea <leahk524@gmail.com>
2023-05-16 01:16:34 +0300

    kk

--- src/components/coupons/couponCard/CouponCard.tsx
+++ src/components/coupons/couponCard/CouponCard.tsx
@@ -1,11 +1,14 @@
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import ICouponsData from "../../../models/ICouponsData";
 import { AppState } from "../../../redux/app-state";
 import EditCoupon from "../editCoupon/editCoupon";
 import './couponCard.css';
 import Modal from 'react-modal';
 import { useState } from "react";
+import axios from "axios";
+import IPurchaseData from "../../../models/IPurchaseData";
+
 
 const customStyles = {
     content: {
@@ -20,10 +23,35 @@ const customStyles = {
 
 // Make sure to bind modal to your appElement (https://reactcommunity.org/react-modal/accessibility/)
 Modal.setAppElement('#root');
+
 function Coupon(props: ICouponsData) {
     let loginData = useSelector((state: AppState) => state.loginData)
     function editCoupon() {
         <EditCoupon id={props.id} name={props.name} price={props.price} description={props.description} startDate={props.startDate} endDate={props.endDate} categoryName={props.categoryName} companyName={props.companyName} amount={props.amount} />
+    }
+    const navigate = useNavigate();
+    let coupon = props;
+    let user = loginData;
+    let userId = loginData?.id;
+    let timeStamp = "2024-02-02T00:00:00.000+00:00";
+    let isBuy = false;
+    async function addToCart(id: number) {
+        if (loginData == null) {
+            navigate("/login")
+            return;
+        }
+        try {
+
+            const response = await axios.post("http://localhost:8080/purchase", { timeStamp, customer:{user}, coupon, isBuy });
+        }
+        catch (e: any) {
+            console.error(e);
+            if (e.response?.data?.error?.message) {
+                alert(e.response.data.error.message)
+            } else {
+                alert("failed to add to cart")
+            }
+        }
     }
 
 
@@ -44,11 +72,9 @@ function Coupon(props: ICouponsData) {
     function closeModal() {
         setIsOpen(false);
     }
-    function buyNow(id:number): void {
-        throw new Error("Function not implemented.");
-    }
 
-    function addToCart(id:number): void {
+
+    function buyNow(id: number): void {
         throw new Error("Function not implemented.");
     }
 
@@ -94,13 +120,14 @@ function Coupon(props: ICouponsData) {
                     <div>
                         {props.endDate}
                     </div>
-                    <button className="button-modal" onClick={event=> addToCart(props.id)}>Add to cart</button>
+                    <button className="button-modal" onClick={event => addToCart(props.id)}>Add to cart</button>
                     <button className="button-modal" onClick={event => buyNow(props.id)}>buy now</button>
                     <div className="edit-button">
                         {loginData?.userType == "admin" && (<input type="button" value={"edit"} onClick={editCoupon} />)}
                     </div>
                 </div>
             </Modal>
+
         </div>
     )
 }
---
+++ src/models/IPurchaseData.ts
@@ -0,0 +1,11 @@
+export default interface IPurchaseData{
+    id: number;
+    userName : String;
+    price: number;
+    couponName : String;
+    categoryName: String;
+    companyName : String;
+    isBuy: boolean;
+
+ 
+}
